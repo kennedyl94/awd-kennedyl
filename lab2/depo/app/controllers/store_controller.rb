8e7a6201c1adbbec85b80bf6ced7bb670ee4efd7
kennedyl <kennedyl@msoe.edu>
2015-04-13 16:13:32 -0500

    added the refresh counter

--- lab2/depo/app/controllers/store_controller.rb
+++ lab2/depo/app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
+  
+  def increment_count
+	#session[:counter] = nil
+	session[:counter] ||= 0
+	session[:counter] += 1
+	
+
+	
+	end
+	
   def index
 	@products = Product.order(:title)
+	
+	increment_count()
+
   end
+  
 end
